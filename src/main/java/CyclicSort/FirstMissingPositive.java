04b466d93bb36fac3bf568353f1820845f4c862f
saiteja8848 <saiteja8848@gmail.com>
2023-03-18 21:33:58 +0530

    cs

---
+++ src/main/java/CyclicSort/FirstMissingPositive.java
@@ -0,0 +1,47 @@
+package CyclicSort;
+
+/**
+* 
+* @author saiteja
+* PraticeLink : https://leetcode.com/problems/first-missing-positive/
+* Time Complexity : O(n)
+* Space Complexity : O(1)
+*
+* same as first missing number - cyclic sort
+* 
+*
+*/
+public class FirstMissingPositive {
+
+	// same as missingNumber
+	public int firstMissingPositive(int[] nums) {
+
+		int i = 0;
+
+		// Cyclic Sort in action - trying to place the elements in their respective
+		// index
+		while (i < nums.length) {
+			int correct = nums[i] - 1;
+			if (nums[i] > 0 && nums[i] <= nums.length && nums[i] != nums[correct]) {
+				int temp = nums[i];
+				nums[i] = nums[correct];
+				nums[correct] = temp;
+			} else
+				i++;
+		}
+
+		// Checking for the missing item in the soted array "nums"
+		for (int z = 0; z < nums.length; z++) {
+			if (nums[z] != z + 1) {
+				return z + 1;
+			}
+		}
+
+		return nums.length + 1;
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+}
