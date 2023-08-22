d4f58992c2a20e0d074ec7daea3e5d3c8267affe
saiteja <47274869+saiteja8848@users.noreply.github.com>
2023-08-22 22:22:33 +0530

    Update JosephusProblem.java

--- src/main/java/recursion/JosephusProblem.java
+++ src/main/java/recursion/JosephusProblem.java
@@ -1,24 +1,36 @@
 package recursion;
 
 
-/**
- * 
- * https://youtu.be/KJsuIMWvN20
- * @author saiteja
- *
- */
-
 public class JosephusProblem {
-	
-	static int josephus(int n, int k) {
-		if(n==1)
-			return 0;
-		return (josephus(n-1,k)+k)%n;
-	}
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
 	}
-
 }
+
+class Solution1589
+{
+    public int josephus(int n, int k){
+        if(n == 1) 
+           return 1;
+        int position = josephus(n-1,k); 
+        return (position + k-1) % n + 1; 
+    }
+    
+
+    public int josephus2(int n, int k){
+      int survivor = 0;
+        for (int i = 2; i <= n; i++) {
+            survivor = (survivor + k) % i;
+        }
+        return survivor + 1;
+    }
+    
+
+     public int josephus3(int n, int k){
+      if(n == 1) 
+          return 0;
+        int position = josephus(n-1,k);     
+        return (position + k) % n; 
+    }
+    
+}
+
