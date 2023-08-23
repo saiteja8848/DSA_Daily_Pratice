84c6a16dd09d1fd67d6c58720b53e4586c7e7fe2
saiteja <47274869+saiteja8848@users.noreply.github.com>
2023-08-23 22:53:38 +0530

    Update DecodeWays.java

--- src/main/java/backtracking/DecodeWays.java
+++ src/main/java/backtracking/DecodeWays.java
@@ -2,9 +2,27 @@ package backtracking;
 
 public class DecodeWays {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+	 Map<Integer, Integer> memo = new HashMap<>();
+    public int numDecodings(String s) {
+        
+        return back(s, 0);
+    }
+    
+    private int back(String s, int start){
+        if(start == s.length()) return 1;
+        
+        if(memo.containsKey(start)) return memo.get(start);
+        
+        int ans = 0;
+        for(int i = start + 1; i <= s.length(); i++){
+            if(Integer.parseInt(s.substring(start, i)) > 26) break;
+            if(Integer.parseInt(s.substring(start, i)) == 0) return 0;
+            
+            ans += back(s, i);
+        }
+        
+        memo.put(start, ans);
+        return ans;
+    }
 
 }
