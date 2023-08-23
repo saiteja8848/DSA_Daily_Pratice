775c17771cff2b58392f3141bcc48fa3ae70f4f9
saiteja <47274869+saiteja8848@users.noreply.github.com>
2023-08-23 22:53:07 +0530

    Update GenerateParenthesis.java

--- src/main/java/backtracking/GenerateParenthesis.java
+++ src/main/java/backtracking/GenerateParenthesis.java
@@ -2,9 +2,24 @@ package backtracking;
 
 public class GenerateParenthesis {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+	public List<String> generateParenthesis(int n) {
+        List<String> solution = new ArrayList<>();
+        generateParenthesis(n,solution,"",0,0);
+        return solution;    
+    }
+    
+    void generateParenthesis(int n, List<String>solution, String result, int open,int close){
+        
+        if(result.length()==n*2){
+            solution.add(result);
+            return;
+        }
+        
+        if(open<n)
+            generateParenthesis(n,solution,result+"(",open+1,close);
+        if(close<open)
+            generateParenthesis(n,solution,result+")",open,close+1);
+        
+    
+    }
 }
