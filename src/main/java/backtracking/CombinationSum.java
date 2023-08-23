73651f55c43dd04a5e364807e225804978f15b79
saiteja <47274869+saiteja8848@users.noreply.github.com>
2023-08-23 22:50:51 +0530

    Update CombinationSum.java

--- src/main/java/backtracking/CombinationSum.java
+++ src/main/java/backtracking/CombinationSum.java
@@ -4,32 +4,104 @@ import java.util.ArrayList;
 import java.util.List;
 
 class CombinationSum {
-	public List<List<Integer>> combinationSum(int[] candidates, int target) {
-		List<List<Integer>> ans = new ArrayList<>();
-		List<Integer> sol = new ArrayList<>();
-		generateCombination(candidates, target, ans, sol, 0);
-		return ans;
-	}
+   public List<List<Integer>> combinationSum(int[] candidates, int target) {
+    List<List<Integer>> list = new ArrayList<>();
+    backtrack(list, new ArrayList<Integer>(), candidates, target, 0);
+    return list;
+}
 
-	public void generateCombination(int[] nums, int target, List<List<Integer>> ans, List<Integer> temp, int index) {
+private void backtrack(List<List<Integer>> list, List<Integer> tempList, int[] cand, int remain, int start) {
+    if (remain < 0) return; /** no solution */
+    else if (remain == 0) list.add(new ArrayList<>(tempList));
+    else{
+        for (int i = start; i < cand.length; i++) { 
+            tempList.add(cand[i]);
+            backtrack(list, tempList, cand, remain-cand[i], i);
+            tempList.remove(tempList.size()-1);
+        } 
+    }
 
-		// base condition
-		if (index == nums.length) {
-			if (target == 0) {
-				ans.add(new ArrayList<>(temp));
-			}
-			return;
-		}
-
-		if (index < nums.length && nums[index] <= target) {
-			temp.add(nums[index]);
-			generateCombination(nums, target - nums[index], ans, temp, index);
-			temp.remove(temp.size() - 1);
-		}
-
-		generateCombination(nums, target, ans, temp, index + 1);
-	}
+}
 
 }
 
 
+
+class CombinationSumII {
+  public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+   List<List<Integer>> list = new LinkedList<List<Integer>>();
+   Arrays.sort(candidates);
+   backtrack(list, new ArrayList<Integer>(), candidates, target, 0);
+   return list;
+}
+
+private void backtrack(List<List<Integer>> list, List<Integer> tempList, int[] cand, int remain, int start) {
+   
+   if(remain < 0) return; /** no solution */
+   else if(remain == 0) list.add(new ArrayList<>(tempList));
+   else{
+      for (int i = start; i < cand.length; i++) {
+         if(i > start && cand[i] == cand[i-1]) continue; /** skip duplicates */
+         tempList.add(cand[i]);
+         backtrack(list, tempList, cand, remain - cand[i], i+1);
+         tempList.remove(tempList.size() - 1);
+      }
+   }
+}
+
+}
+
+
+class CombinationSumIII {
+    public List<List<Integer>> combinationSum3(int k, int n) {
+    List<List<Integer>> list = new ArrayList<>();
+    backtrack(list, new ArrayList<Integer>(), k, n, 1);
+    return list;
+}
+
+private void backtrack(List<List<Integer>> list, List<Integer> tempList, int k, int remain, int start) {
+    if(tempList.size() > k) return; /** no solution */
+    else if(tempList.size() == k && remain == 0) list.add(new ArrayList<>(tempList));
+    else{
+        for (int i = start; i <= 9; i++) {
+            tempList.add(i);
+            backtrack(list, tempList, k, remain-i, i+1);
+            tempList.remove(tempList.size() - 1);
+        }
+    }
+}
+
+	
+}
+
+
+class CombinationSumIV {
+   HashMap<Integer , Integer> cache = new HashMap<>();
+
+public int combinationSum4(int[] candidates, int target) {
+
+    return  dfs(candidates ,target );
+}
+
+public int dfs (int[] candidates , int currentValue){
+
+    if (cache.containsKey(currentValue)){
+       return cache.get(currentValue);
+    }
+
+    int count = 0;
+    if (currentValue == 0){
+        return ++count;
+    }
+
+
+    for (int candidate : candidates) {
+        if (currentValue - candidate >= 0) {
+            count += dfs(candidates, currentValue - candidate);
+        }
+    }
+    cache.put(currentValue,count);
+
+    return count;
+}
+}
