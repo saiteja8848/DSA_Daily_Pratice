695d683b10986a8ce3ac4ff107773e0752f0abdd
saiteja <47274869+saiteja8848@users.noreply.github.com>
2023-08-23 22:55:52 +0530

    Update RestoreIpAddress.java

--- src/main/java/backtracking/RestoreIpAddress.java
+++ src/main/java/backtracking/RestoreIpAddress.java
@@ -2,9 +2,24 @@ package backtracking;
 
 public class RestoreIpAddress {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+	 List<String> ans = new ArrayList<>();
+    void backtrack(String s, String path, int index, int count) {
+        if (count > 4) return;
+        if (count == 4 && index >= s.length()) {
+            ans.add(path.substring(0,path.length()-1));
+            return;
+        }
+        for (int i = 1; i <= 3 && index + i <= s.length(); i++) {
+            String num = s.substring(index, index + i);
+            if (num.charAt(0) == '0' && i != 1) break;
+            else if (Integer.parseInt(num) <= 255) {
+                backtrack(s, path + s.substring(index, index + i) + ".", index + i,count + 1);
+            }
+        }
+    }
+    public List<String> restoreIpAddresses(String s) {
+        backtrack(s, "", 0, 0);
+        return ans;
+    } 
 
 }
