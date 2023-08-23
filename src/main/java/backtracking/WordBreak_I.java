895329cff2ac636c4f90eaf130cae58a039126b6
saiteja <47274869+saiteja8848@users.noreply.github.com>
2023-08-23 23:03:24 +0530

    Update WordBreak_I.java

--- src/main/java/backtracking/WordBreak_I.java
+++ src/main/java/backtracking/WordBreak_I.java
@@ -1,5 +1,38 @@
 package backtracking;
 
 public class WordBreak_I {
+     public boolean wordBreak(String s, List<String> wordDict) {
+       //For Memoization
+        HashMap<String,Boolean> map = new HashMap<>();
+        return helper(s, wordDict, map);
+    }
+    
 
+public boolean helper(String target, List<String> wordDict,HashMap<String,Boolean> map){
+              
+        if(map.containsKey(target))
+             return map.get(target);
+        
+        
+        //If the target lenght is empty, it means we have parsed the string and it is avaliable in the dictionary right
+        if(target.isEmpty())
+            return true;
+       
+        for(String word: wordDict){
+            
+             System.out.println(target);
+            if(target.startsWith(word)){
+               
+                if(helper(target.substring(word.length()),wordDict, map)){
+                    //System.out.println(target);
+                    map.put(target,true);
+                    return true;
+                }
+                
+            }
+       }
+        
+        map.put(target,false);
+        return false;   
+    }
 }
