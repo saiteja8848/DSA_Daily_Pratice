f4342f6777a886ed448878dbecd46f67bf221f00
saiteja <47274869+saiteja8848@users.noreply.github.com>
2023-08-23 23:02:54 +0530

    Update WordBreak_II.java

--- src/main/java/backtracking/WordBreak_II.java
+++ src/main/java/backtracking/WordBreak_II.java
@@ -1,5 +1,31 @@
 package backtracking;
 
 public class WordBreak_II {
-
+        Map<String, List<String>> mem = new HashMap<>();
+    
+    public List<String> wordBreak(String s, List<String> wordDict) {
+        
+        if(mem.get(s)!=null)
+             return mem.get(s);
+        
+        List<String> result = new ArrayList<>();
+        
+        if(wordDict.contains(s))
+            result.add(s);
+        
+        
+        for(int i=1;i<s.length();i++){
+            String word = s.substring(0,i);
+            if(wordDict.contains(word)){
+                List<String> tmp = wordBreak(s.substring(i),wordDict);
+                for(String str:tmp){
+                    result.add(word+" "+str);
+                }
+            }
+        }
+        mem.put(s,result);
+        return result;
+        
+        
+    }
 }
