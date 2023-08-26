8a8b1653e34a0555933c447a4db928148e6225be
saiteja <47274869+saiteja8848@users.noreply.github.com>
2023-08-26 20:28:40 +0530

    Update RotateArray.java

--- src/main/java/Arrays/RotateArray.java
+++ src/main/java/Arrays/RotateArray.java
@@ -19,3 +19,38 @@ public void reverse(int[] nums, int start, int end) {
 }
 
 }
+
+
+/*
+Compare all neignbour elements (a,b) in A,
+the case of a > b can happen at most once.
+
+Note that the first element and the last element are also connected.
+
+If all a <= b, A is already sorted.
+If all a <= b but only one a > b,
+we can rotate and make b the first element.
+Other case, return false.
+
+
+Complexity
+Time O(n)
+Space O(1)
+
+*/
+
+
+class RotateArray_II {
+  public boolean check(int[] A) {
+        int k = 0, n = A.length;
+        for (int i = 0; i < n; ++i) {
+            if (A[i] > A[(i + 1) % n]) {
+                k++;
+            }
+            if (k > 1) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
