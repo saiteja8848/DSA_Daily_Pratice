c964f6f57e2b8b6089829283f6413bf5c30509fe
saiteja <47274869+saiteja8848@users.noreply.github.com>
2023-08-26 20:24:12 +0530

    Update ExcelSheet.java

--- src/main/java/Arrays/ExcelSheet.java
+++ src/main/java/Arrays/ExcelSheet.java
@@ -1,5 +1,24 @@
 package Arrays;
 
 public class ExcelSheet {
+      public String convertToTitle(int columnNumber) {
+        String ans = "";
+        
+        while(columnNumber > 0) {
+            columnNumber--;
+            ans= (char)((columnNumber) % 26 + 'A') + ans;
+            columnNumber = (columnNumber) / 26;
+        }
+        return ans;
+    }
 
+    public int titleToNumber(String columnTitle) {
+        int result=0;
+
+        for(char ch:columnTitle.toCharArray()){
+            result*=26;
+            result+=ch-'A'+1;
+        }
+       return result;
+    }
 }
