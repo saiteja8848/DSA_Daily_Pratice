4f1817a53ed17b8a21ca10faa7da57453d334007
saiteja <47274869+saiteja8848@users.noreply.github.com>
2023-08-26 20:17:00 +0530

    Update FractionToRecurringDecimal.java

--- src/main/java/Arrays/FractionToRecurringDecimal.java
+++ src/main/java/Arrays/FractionToRecurringDecimal.java
@@ -1,5 +1,40 @@
 package Arrays;
 
 public class FractionToRecurringDecimal {
+    public String fractionToDecimal(int num, int denom) {
+        if(denom==0)
+          return "undefined";
+        if(num==0)
+           return "0";
 
+        String sign = ((num>0)^(denom>0))?"-":"";
+        
+        long numerator = Math.abs((long)num);
+        long denominator = Math.abs((long)denom);
+
+        StringBuilder result = new StringBuilder();
+        result.append(sign);
+        result.append(numerator/denominator);
+      
+        numerator=numerator%denominator;
+        if(numerator==0)
+         return result.toString();
+        result.append(".");
+      
+        Map<Long,Integer> trackQuotient = new HashMap<>();
+        trackQuotient.put(numerator , result.length());
+        while(numerator!=0){
+            numerator*=10;
+            result.append(numerator/denominator);
+            numerator=numerator%denominator;
+            if(trackQuotient.containsKey(numerator)){
+                int index = trackQuotient.get(numerator);
+                result.insert(index,"(");
+                result.append(")");
+                break;
+            }else
+              trackQuotient.put(numerator,result.length());
+        }
+      return result.toString();
+    }
 }
