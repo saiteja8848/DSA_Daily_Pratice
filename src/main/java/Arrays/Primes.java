3a8f7f3c5c746c1c516e66c5df56898cad4d4fc4
saiteja <47274869+saiteja8848@users.noreply.github.com>
2023-08-26 20:19:39 +0530

    Update Primes.java

--- src/main/java/Arrays/Primes.java
+++ src/main/java/Arrays/Primes.java
@@ -23,4 +23,27 @@ public class Primes {
         }
         return dividend;
     }
+
+
+   // Counting primies
+       public int countPrimes(int n) {
+          if(n==0 || n == 1) return 0;
+        
+        boolean[] prime = new boolean[n];
+        //initialising notPrime with a value 1(counting "1" as not prime)
+        int notPrime = 1;
+        
+        for(int i=2;i*i<n;i++){
+            if(prime[i] == false){
+                for(int j=i;j*i<n;j++){
+                    if(prime[j*i] == false){
+                        notPrime++;
+                        prime[j*i] = true;
+                    }
+                }
+            }
+        }
+        return n - notPrime - 1;
+    }
+     
 }
