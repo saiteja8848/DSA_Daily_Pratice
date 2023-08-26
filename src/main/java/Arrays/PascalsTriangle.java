97dd91c9e93d0b8710efa4724c4d313cec7c75ec
saiteja <47274869+saiteja8848@users.noreply.github.com>
2023-08-26 20:21:38 +0530

    Update PascalsTriangle.java

--- src/main/java/Arrays/PascalsTriangle.java
+++ src/main/java/Arrays/PascalsTriangle.java
@@ -1,5 +1,17 @@
 package Arrays;
 
 public class PascalsTriangle {
-
+       public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> answer = new ArrayList<List<Integer>>();
+        List<Integer> row, pre=null;        
+        for(int i=0;i<numRows;i++){
+            row = new ArrayList();
+            for(int j=0;j<=i;j++){
+                row.add((j==0||j ==i) ? 1 :pre.get(j-1)+pre.get(j));
+            }
+            pre=row;
+            answer.add(row);   
+        }
+        return answer;    
+    }
 }
