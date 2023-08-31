964a975de5032eab1bfec334873f246218930d4f
saiteja <47274869+saiteja8848@users.noreply.github.com>
2023-08-31 21:38:13 +0530

    Update DisjointSet.java

--- src/main/java/Graphs/DisjointSet.java
+++ src/main/java/Graphs/DisjointSet.java
@@ -1,5 +1,12 @@
 package Graphs;
 
 public class DisjointSet {
-
+    	int find(int A[],int X){
+        return A[X]==X ? X : find(A,A[X]);
+	}
+	void unionSet(int A[],int X,int Z){
+       int xParent = find(A,X);
+       int yParent = find(A,Z);
+       A[xParent]=yParent;
+	}
 }
