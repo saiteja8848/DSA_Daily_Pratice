cf5bd6e587da1a19dac40d203787998eef1b6d57
saiteja <47274869+saiteja8848@users.noreply.github.com>
2023-09-04 23:18:16 +0530

    Update GasStationOrCircularTour.java

--- src/main/java/Greedy/GasStationOrCircularTour.java
+++ src/main/java/Greedy/GasStationOrCircularTour.java
@@ -1,37 +1,25 @@
 package Greedy;
 
 public class GasStationOrCircularTour {
+  public int canCompleteCircuit(int[] gas, int[] cost) {
+        int totalGas = Arrays.stream(gas).sum();
+        int totalCost = Arrays.stream(cost).sum();
 
-	public int canCompleteCircuit(int[] gas, int[] cost) {
-		int totalGas = 0;
-		int totalCost = 0;
-		for (int i = 0; i < gas.length; i++) {
-			totalGas += gas[i];
-			totalCost += cost[i];
-		}
-
-		if (totalGas < totalCost)
-			return -1;
-
-		// where to start
-		int remainingGas = 0;
-		int start = 0;
-
-		for (int i = 0; i < gas.length; i++) {
-			remainingGas += gas[i] - cost[i];
-			if (remainingGas < 0) {
-				start = i + 1;
-				remainingGas = 0;
-			}
-		}
-
-		return start;
-
-	}
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+        if(totalGas<totalCost)
+            return -1;
+        
+        int remainingGas=0;
+        int start=0;
+        
+        for(int i=0;i<gas.length;i++){
+            remainingGas+=gas[i]-cost[i];
+            if(remainingGas<0){
+                start=i+1;
+                remainingGas=0;
+            }
+        }
+        
+        return start;
+    }
 
 }
