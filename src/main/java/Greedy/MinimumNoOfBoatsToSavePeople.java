92f45b89d738e418dc1a48ffdcb67cc6bf9e1d09
saiteja <47274869+saiteja8848@users.noreply.github.com>
2023-09-04 23:54:29 +0530

    Update MinimumNoOfBoatsToSavePeople.java

--- src/main/java/Greedy/MinimumNoOfBoatsToSavePeople.java
+++ src/main/java/Greedy/MinimumNoOfBoatsToSavePeople.java
@@ -4,30 +4,17 @@ import java.util.Arrays;
 
 public class MinimumNoOfBoatsToSavePeople {
 
-	public int numRescueBoats(int[] people, int limit) {
-		int boatCount = 0;
-		Arrays.sort(people);
-
-		int left = 0;
-		int right = people.length - 1;
-
-		while (left <= right) {
-			int sum = people[left] + people[right];
-			if (sum <= limit) {
-				boatCount++;
-				left++;
-				right--;
-			} else {
-				boatCount++;
-				right--;
-			}
-		}
-		return boatCount;
-	}
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+  public int numRescueBoats(int[] people, int limit) {
+        int boatCount = 0;
+        Arrays.sort(people);
+        int left = 0;
+        int right = people.length - 1;
+        while(left <= right){
+             left = (people[left] + people[right])<=limit? left+1: left;
+             boatCount++;
+             right--;
+        }
+        return boatCount;
+    }
 
 }
