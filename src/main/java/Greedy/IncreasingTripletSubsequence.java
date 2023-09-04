6a1690f7a8e187281aea6d15e1943a4477296b2f
saiteja <47274869+saiteja8848@users.noreply.github.com>
2023-09-05 00:10:18 +0530

    Update IncreasingTripletSubsequence.java

--- src/main/java/Greedy/IncreasingTripletSubsequence.java
+++ src/main/java/Greedy/IncreasingTripletSubsequence.java
@@ -1,27 +1,20 @@
 package Greedy;
 
 public class IncreasingTripletSubsequence {
+   public boolean increasingTriplet(int[] nums) {
+    int a = Integer.MAX_VALUE;
+    int b = Integer.MAX_VALUE;
 
-	public boolean increasingTriplet(int[] nums) {
+    for (int num : nums) {
+        if (num <= a) {
+            a = num;
+        } else if (num <= b) {
+            b = num;
+        } else {
+            return true;
+        }
+    }
 
-		int a = Integer.MAX_VALUE;
-		int b = Integer.MAX_VALUE;
-
-		for (int i = 0; i < nums.length; i++) {
-			if (nums[i] <= a) {
-				a = nums[i];
-			} else if (nums[i] <= b) {
-				b = nums[i];
-			} else
-				return true;
-		}
-
-		return false;
-	}
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+    return false;
+}
 }
