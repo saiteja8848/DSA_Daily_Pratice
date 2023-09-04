857064a6f7e2aab77cbb03ba6b3992edf6c82f93
saiteja <47274869+saiteja8848@users.noreply.github.com>
2023-09-04 16:40:59 +0530

    Update MinimumNoOfPlatformsRequired.java

--- src/main/java/Greedy/MinimumNoOfPlatformsRequired.java
+++ src/main/java/Greedy/MinimumNoOfPlatformsRequired.java
@@ -2,9 +2,31 @@ package Greedy;
 
 public class MinimumNoOfPlatformsRequired {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+	 static int findPlatform(int arr[], int dep[], int n)
+    {
+        Arrays.sort(arr); 
+        Arrays.sort(dep); 
+  
+        int plat_needed = 1, result = 1; 
+        int i = 1, j = 0; 
+  
+        while (i < n && j < n) { 
+        
+            if (arr[i] <= dep[j]) { 
+                plat_needed++; 
+                i++; 
+            } 
+  
+            else if (arr[i] > dep[j]) { 
+                plat_needed--; 
+                j++; 
+            } 
+  
+            if (plat_needed > result) 
+                result = plat_needed; 
+        } 
+  
+        return result; 
+    }
 
 }
