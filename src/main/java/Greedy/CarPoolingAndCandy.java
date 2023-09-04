1a4c6ffd0c0d4a2a63cd801541f72ceba3652c52
saiteja <47274869+saiteja8848@users.noreply.github.com>
2023-09-04 16:37:22 +0530

    Update CarPoolingAndCandy.java

--- src/main/java/Greedy/CarPoolingAndCandy.java
+++ src/main/java/Greedy/CarPoolingAndCandy.java
@@ -1,5 +1,31 @@
 package Greedy;
 
 public class CarPoolingAndCandy {
-
+    public int candy(int[] ratings) {
+        int n =  ratings.length;
+        if(n==0)
+            return 0;
+        int left[] = new int[n];
+        Arrays.fill(left,1);
+        for(int i=1;i<n;i++)
+        {
+            if(ratings[i] > ratings[i-1])
+                left[i] = left[i-1]+1;
+        }
+        int right=1;
+        for(int i=n-2;i>=0;i--)
+        {
+            if(ratings[i] > ratings[i+1]){
+                right++;
+                  left[i] = Math.max(right,left[i]);
+            }
+              
+            else
+              right=1;
+        }
+        int ans = 0;
+        for(int i=0;i<n;i++)
+            ans += left[i];
+        return ans;
+    }
 }
