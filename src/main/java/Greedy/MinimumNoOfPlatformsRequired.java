09ce999b7168d84c18f4ad663d83058655f4c729
saiteja <47274869+saiteja8848@users.noreply.github.com>
2023-09-04 23:57:39 +0530

    Update MinimumNoOfPlatformsRequired.java

--- src/main/java/Greedy/MinimumNoOfPlatformsRequired.java
+++ src/main/java/Greedy/MinimumNoOfPlatformsRequired.java
@@ -1,32 +1,30 @@
 package Greedy;
 
 public class MinimumNoOfPlatformsRequired {
-
-	 static int findPlatform(int arr[], int dep[], int n)
+   static int findPlatform(int arr[], int dep[], int n)
     {
-        Arrays.sort(arr); 
-        Arrays.sort(dep); 
-  
-        int plat_needed = 1, result = 1; 
-        int i = 1, j = 0; 
-  
-        while (i < n && j < n) { 
-        
-            if (arr[i] <= dep[j]) { 
-                plat_needed++; 
-                i++; 
-            } 
-  
-            else if (arr[i] > dep[j]) { 
-                plat_needed--; 
-                j++; 
-            } 
-  
-            if (plat_needed > result) 
-                result = plat_needed; 
-        } 
-  
-        return result; 
+        Arrays.sort(arr);
+    Arrays.sort(dep);
+
+    int plat_needed = 1; // Initialize the count of platforms needed to 1
+    int result = 1; // Initialize the result to 1
+    int i = 1, j = 0;
+
+    // Use a single loop to traverse both arrival and departure arrays
+    while (i < n && j < n) {
+        // If the next event is an arrival, increment the number of platforms needed
+        if (arr[i] <= dep[j]) {
+            plat_needed++;
+            i++;
+        } else { // If the next event is a departure, decrement the number of platforms needed
+            plat_needed--;
+            j++;
+        }
+
+        // Update the result with the maximum number of platforms needed at any time
+        result = Math.max(result, plat_needed);
     }
 
+    return result;
+    }
 }
