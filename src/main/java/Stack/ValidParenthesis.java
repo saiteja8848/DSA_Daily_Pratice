d58a4f9d8fef0738890429317112ccac24da7d34
saiteja <47274869+saiteja8848@users.noreply.github.com>
2023-09-11 23:24:34 +0530

    Update ValidParenthesis.java

--- src/main/java/Stack/ValidParenthesis.java
+++ src/main/java/Stack/ValidParenthesis.java
@@ -1,5 +1,24 @@
 package Stack;
 
 public class ValidParenthesis {
-
+      public boolean isValid(String s) {
+        if(s==null||s.length()%2==1)
+            return false;
+        if(s.isEmpty())
+			return true;
+       Stack<Character> stack = new Stack<Character>();
+        for(char c:s.toCharArray()){
+            if(c=='(')
+                stack.push(')');
+            else if(c=='[')
+                stack.push(']');
+            else if(c=='{')
+                stack.push('}');
+            else 
+                 if(stack.isEmpty()||stack.pop()!=c)
+                return false; 
+        }
+        
+        return stack.isEmpty();  
+    }
 }
